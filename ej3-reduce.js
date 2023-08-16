6c5b731a4cd72b082684dcd11714b3d6a16bff9a
cristianderudi <skorpiano@gmail.com>
2023-08-16 20:20:53 -0300

    Primer commit

---
+++ ej3-reduce.js
@@ -0,0 +1,5 @@
+/* Nos dejaron un array con las vueltas que dio una corredora entrenando en distintos momentos para una maratón.
+
+Nuestro desafío será crear la variable totalVueltas y almacenar en ella el total, usando el método reduce. */
+
+let vueltas = [5, 8, 12, 3, 22];
